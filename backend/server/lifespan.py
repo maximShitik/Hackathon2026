5bad78ff59b91171815d2bee939c7dbf46ea69c7
rotem <psyberryd@gmail.com>
2026-01-19 17:39:25 +0200

    Add server.

---
+++ backend/server/lifespan.py
@@ -0,0 +1,136 @@
+"""
+This module is used to set up the database pool and response provider in the app lifespan.
+
+Functions:
+    lifespan: Sets up the database pool and response provider in the app lifespan.
+"""
+from contextlib import asynccontextmanager
+from datetime import datetime
+
+from fastapi import FastAPI
+import httpx
+import os
+import json
+from typing import Dict, Any
+from backend.model.gemini.client import GeminiToolUsageClient, SimpleGeminiResponseProvider
+from backend.model.gemini.summary_format_provider import create_gemini_summary_format
+from backend.model.openai.client import OpenAIToolResponseProvider, OpenAISimpleResponseProvider
+from backend.model.openai.tool_usage_parser import create_summary_format
+from backend.model.openai.chat_utils import count_tokens
+from backend.model.tool_manager import ToolManager
+from backend.model.chat_manager import ChatManager
+from backend.db.init_db import init_db_if_needed
+from backend.db.sqlite_connection import get_db
+from backend.db.single_instance_pool import SingleInstancePool
+from backend.model.response_provider import ResponseProvider
+from backend.server.ad_provider import MockAdProvider
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+async def push_to_novisign_async(data_items: Dict[str, Dict[str, Any]], *, api_key: str,
+                                 studio_domain: str = "app.novisign.com",
+                                 items_group: str, timeout: int = 10) -> Dict[str, Any]:
+    url = f"https://{studio_domain}/catalog/items/{items_group}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": api_key,
+    }
+
+    payload = {
+        "data": data_items
+    }
+
+    async with httpx.AsyncClient(timeout=timeout) as client:
+        response = await client.post(
+            url,
+            json=payload,
+            headers=headers,
+        )
+
+    if response.status_code >= 400:
+        raise RuntimeError(
+            f"NoviSign API error {response.status_code}: {response.text}"
+        )
+
+    return {
+        "success": True,
+        "items_updated": len(data_items),
+        "timestamp": datetime.utcnow().isoformat(),
+        "response": response.json(),
+    }
+
+
+def _setup_openai_provider(cfg: Dict[str, str], tool_manager: ToolManager) -> ResponseProvider:
+    """
+    Sets up the OpenAI provider from the given cfg and tool_manager.
+
+    :param cfg: The cfg to use.
+    :param tool_manager: The tool manager to use.
+    :return: The OpenAI provider.
+    """
+    api_key = cfg["OPENAI_API_KEY"]
+    model_name = cfg.get("MODEL_NAME", "gpt-5")
+    tool_response_provider = OpenAIToolResponseProvider(tool_manager=tool_manager,
+                                                        api_key=api_key, model_name=model_name)
+    simple_response_provider = OpenAISimpleResponseProvider(api_key, model_name)
+    return ChatManager(tool_response_provider, simple_response_provider, create_summary_format,
+                       token_counter=count_tokens)
+
+
+def _setup_gemini_provider(cfg: Dict[str, str], tool_manager: ToolManager) -> ResponseProvider:
+    api_key = cfg["GEMINI_KEY"]
+    model_name = cfg.get("GEMINI_MODEL", "gemini-2.0-flash")
+    tool_response_provider = GeminiToolUsageClient(tool_manager=tool_manager, api_key=api_key,
+                                                   model_name=model_name)
+    simple_response_provider = SimpleGeminiResponseProvider(api_key, model_name)
+    return ChatManager(tool_response_provider, simple_response_provider,
+                       create_gemini_summary_format,
+                       token_counter=None)
+
+
+def _setup_response_provider(tool_manager: ToolManager) -> ResponseProvider:
+    """
+    Sets up the response provider from the cfg.json file and the given ToolManager.
+
+    :param tool_manager: The tool manager to use.
+    :return: The response provider.
+    """
+    cfg_path = "cfg.json"  # Path from root folder.
+    if not os.path.exists(cfg_path):
+        raise RuntimeError(r"To prevent the API key leaking, the CFG file was not added to this "
+                           r"repository. In order to run this server, find this function "
+                           r"'setup_response_provider' in the file backend\server\main.py and "
+                           r"hard code the api_key in the respective variable. Alternatively, "
+                           r"create a cfg.json file with the field 'OPENAI_API_KEY' containing "
+                           r"the key for open AI usage, or 'GEMINI_KEY' for gemini usage..")
+    with open(cfg_path) as f:
+        cfg = json.load(f)
+    setup_method = _setup_openai_provider
+    return setup_method(cfg, tool_manager)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Sets up the database pool and response provider in the app lifespan.
+    """
+    cfg_path = "cfg.json"
+    with open(cfg_path) as f:
+        app.state.use_openAI = json.load(f).get("USE_OPENAI", False)
+    app.state.db_pool = SingleInstancePool(get_db)
+    init_db_if_needed(app.state.db_pool.get_connection)
+    app.state.ad_provider = MockAdProvider(app.state.db_pool.get_connection())
+    app.state.response_provider = _setup_response_provider(ToolManager(app.state.db_pool))
+    scheduler = AsyncIOScheduler()
+    async def scheduled_update():
+        return
+        data = app.state.ad_provider.get_ad()
+        await push_to_novisign_async(api_key="", items_group="mall-signage", data_items=data, )
+
+    scheduler.add_job(scheduled_update, trigger="interval", seconds=30, max_instances=1,
+                      coalesce=True)
+    scheduler.start()
+    app.state.messages = []
+    yield
+    app.state.db_pool.close()
+    scheduler.shutdown()
---
+++ backend/server/main.py
@@ -0,0 +1,107 @@
+"""
+Server running the chat.
+"""
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+from fastapi.staticfiles import StaticFiles
+import httpx
+from typing import Dict, Any
+from datetime import datetime
+import traceback
+from backend.model.message import Message, MessageMetadata
+from backend.model.response_provider import ResponseProvider
+from backend.server.lifespan import lifespan
+from backend.server.sse_factory import *
+from backend.model.chat_events_factory import ChatEventType
+from backend.server.ad_provider import AdProvider
+
+# The application server.
+app = FastAPI(lifespan=lifespan)
+
+# General instructions to provide the agent.
+SYSTEM_INSTRUCTIONS = """
+You are a assist people at the mall. They may ask you for directions, what stores are there in 
+the mall and what products do they sell. Use tools to answe these queries, do not invent or guess 
+any data about any store, stick to the tools output.
+Always be courteous and kind, but stick to providing answers about the mall, do not answer any 
+other topic. Some products and stores have a coupon, suggest users to lookup if there's any 
+available coupon for the store/product they are looking for.
+"""
+
+
+@app.get("/model_name")
+async def get_config():
+    return {"name": "assistant" if app.state.use_openAI else "model"}
+
+
+def handle_tool_event(event):
+    if event.data["name"] == "search_product":
+        data = event.data["output"]
+        print(data)
+
+
+@app.post("/chat/stream")
+async def chat_stream(req: Request):
+    """
+    Handles the chat stream post with the given request.
+
+    :param req: The HTTP Request.
+    """
+    body = await req.json()
+    conversation = body.get("conversation", [])
+    messages = app.state.messages
+    # model_name_in_messages = "assistant" if app.state.use_openAI else "model"
+    model_name_in_messages = "assistant"
+
+    async def run():
+        response_provider: ResponseProvider = app.state.response_provider
+        try:
+            placeholder_id = f"placeholder-{len(conversation)}"
+            message_id = f"msg-{len(conversation)}"
+            user_message = Message(**conversation[-1])
+            messages.append(user_message)
+            first_delta = True
+            async for event in response_provider.get_response(messages, SYSTEM_INSTRUCTIONS):
+                event_type = event.type
+                if event_type == ChatEventType.TOOL_CALL_GENERATED:
+                    yield tool_call_generated_event(event.data["name"], event.data["args"])
+                if event_type == ChatEventType.TOOL_OUTPUT_GENERATED:
+                    handle_tool_event(event)
+                    yield tool_output_generated_event(event.data["name"], event.data["output"])
+                if event_type == ChatEventType.PLACEHOLDER:
+                    replace_id = placeholder_id if first_delta else None
+                    yield UI_update_event(SSEEventTypes.RENDER, placeholder_id, "message",
+                                          model_name_in_messages,
+                                          text=event.data["content"], replace_id=replace_id)
+                elif event_type == ChatEventType.TEXT_DELTA:
+                    if first_delta:
+                        yield UI_update_event(SSEEventTypes.RENDER, message_id, "message",
+                                              model_name_in_messages,
+                                              replace_id=placeholder_id, text=event.data["content"])
+                        first_delta = False
+                    else:
+                        yield UI_update_event(SSEEventTypes.PATCH, message_id, "message",
+                                              model_name_in_messages,
+                                              op="append_text", text=event.data["content"])
+                elif event_type == ChatEventType.TEXT_DONE:
+                    meta = event.data.get("metadata", None)
+                    metadata = MessageMetadata(**meta) if meta is not None else None
+                    messages.append(Message(role=event.data.get("role"),
+                                            content=event.data.get("content"), metadata=metadata))
+                    if first_delta:
+                        yield UI_update_event(SSEEventTypes.RENDER, message_id, "message",
+                                              model_name_in_messages,
+                                              replace_id=placeholder_id,
+                                              text=event.data.get("content"))
+                        first_delta = False
+
+            yield done_event()
+        except Exception as e:
+            print(traceback.format_exc())
+            yield error_event(e)
+            yield done_event()
+
+    return StreamingResponse(run(), media_type="text/event-stream")
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
